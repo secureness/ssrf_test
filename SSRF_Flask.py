8db64ad9e46af8c8fe99d35c7734e84345556381
SandBox <86597176+secureness@users.noreply.github.com>
2022-05-22 12:06:15 +0430

    Create SSRF_Flask.py

---
+++ SSRF_Flask.py
@@ -0,0 +1,22 @@
+from itertools import count
+import time
+import flask
+from flask import request
+from flask import Response
+import requests
+
+app = flask.Flask(__name__.split('.')[0])
+
+@app.route("/ssrf",methods = ['HEAD','POST', 'GET'])
+def ssrf():
+    url = request.args.get('url2')
+    response = requests.get("http://127.0.0.1:8080/test")
+    return response
+
+@app.route("/test",methods = ['HEAD','POST', 'GET'])
+def test():
+    return "test test test"
+
+if __name__ == "__main__":
+    #app.run(ssl_context=('cert.pem', 'key.pem'))
+    app.run(debug=True)
