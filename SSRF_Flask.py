c5391842a9459dd9ef37f98834e96c364757dae0
SandBox <86597176+secureness@users.noreply.github.com>
2022-05-22 12:09:41 +0430

    Update SSRF_Flask.py

--- SSRF_Flask.py
+++ SSRF_Flask.py
@@ -10,7 +10,9 @@ app = flask.Flask(__name__.split('.')[0])
 @app.route("/ssrf",methods = ['HEAD','POST', 'GET'])
 def ssrf():
     url = request.args.get('url2')
-    response = requests.get("http://127.0.0.1:8080/test")
+    if not url:
+        url = "http://127.0.0.1:8080/test"
+    response = requests.get(url)
     return response
 
 @app.route("/test",methods = ['HEAD','POST', 'GET'])
