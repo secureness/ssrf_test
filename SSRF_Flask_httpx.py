5d1d2d986ace74f503479620db898e6c2b981b3b
SandBox <86597176+secureness@users.noreply.github.com>
2022-06-03 19:54:21 +0430

    Create SSRF_Flask_httpx.py

---
+++ SSRF_Flask_httpx.py
@@ -0,0 +1,25 @@
+from itertools import count
+import time
+import flask
+from flask import request
+from flask import Response
+import requests
+import httpx
+
+app = flask.Flask(__name__.split('.')[0])
+
+@app.route("/ssrf",methods = ['HEAD','POST', 'GET'])
+def ssrf():
+    url = request.args.get('url2')
+    if not url:
+        url = "http://127.0.0.1:8080/test"
+    response = httpx.get(url)
+    return response
+
+@app.route("/test",methods = ['HEAD','POST', 'GET'])
+def test():
+    return "test test test"
+
+if __name__ == "__main__":
+    #app.run(ssl_context=('cert.pem', 'key.pem'))
+    app.run(debug=False)
